571a05aa046a2c9342a5b12961e3b1ae8eb68121
eliizquierdo <eizquierdo@docente.ceibal.edu.uy>
2023-05-28 19:55:26 -0300

    ppp

---
+++ primerEjemplo/Principal.java
@@ -0,0 +1,12 @@
+package primerEjemplo;
+
+public class Principal{
+
+    public static void main(String[] args){
+        System.out.println("-----------------------------------------------");
+        System.out.println("Hola Martìn");
+
+
+        System.out.println("-----------------------------------------------");
+    }
+}
